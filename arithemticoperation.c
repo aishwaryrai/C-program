69452454fed5950d07cbc7b7cd7fc45f24d00256
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-07-11 16:49:24 +0530

    Add files via upload

---
+++ arithemticoperation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    /*printf("%lu", sizeof(float));*/
+    int a , b;
+    a=6;
+    b=2;
+
+    // printf("a+b=%d\n",a+b);
+    printf("a-b=%d\n", a-b);
+    printf("a*b=%d\n",a*b);
+    printf("a/b=%d\n",a/b);
+    printf("a%b=%d\n",a%b);
+    return 0;
+
+}
