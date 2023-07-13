ad17d010a6f0e208df2633373abf18173d92fd90
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-07-14 01:16:31 +0530

    origin

---
+++ arthemicoperationonfloat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a = 12;
+    float b = 5.7;
+
+    printf("a + b = %f\n", a+b);
+    printf("a - b = %f\n", a-b);
+    printf("a * b = %f\n", a*b);
+    printf("a / b = %f\n", a/b); 
+    return 0;
+
+}
