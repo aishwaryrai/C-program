2868984656bfe9b6c93bbe840fc27841fa66ef9d
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-07-15 00:35:58 +0530

    first

---
+++ question1.c
@@ -0,0 +1,21 @@
+//Print multiplication table of a number entered by the user in pretty form
+
+#include <stdio.h>
+int main(){
+    int a=4;
+    printf("Table of 4\n");
+
+    printf("4*1=%d\n",a*1) ;
+    printf("4*2=%d\n",a*2) ;
+    printf("4*3=%d\n",a*3) ;
+    printf("4*4=%d\n",a*4) ;
+    printf("4*5=%d\n",a*5) ;
+    printf("4*6=%d\n",a*6) ;
+    printf("4*7=%d\n",a*7) ;
+    printf("4*8=%d\n",a*8) ;
+    printf("4*9=%d\n",a*9) ;
+    printf("4*10=%d\n",a*10) ;
+
+    return 0;
+
+}
