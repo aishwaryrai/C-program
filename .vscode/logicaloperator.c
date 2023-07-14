5be58247262dbff2e180b7354e8e22ca6b99dbb8
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-07-14 23:46:22 +0530

    first commit

---
+++ .vscode/logicaloperator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=24;
+    int b=22;
+    printf("a&&b=%d\n",a&&b); //logicalandoperator
+    printf("a||b=%d\n",a||b); //logicaloroperator
+    printf("!(a&&b)=%d\n",!(a&&b)); //logicalnotoperator
+    return 0;
+
+}
