24472fe5d44927eacb75678c8729d396a1762263
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-07-15 00:11:27 +0530

    first

---
+++ assignmentoperator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int a=5; //simple assignment operator
+
+    printf("a=%d\n",a+=1); //add assignment operator
+
+    int b=4;
+
+    printf("b=%d\n",b-=2);  //subtract assignment operator
+
+    int c=8;
+
+    printf("c=%d\n",c*=3); // multiply assignment operator
+
+    int d=10;
+
+    printf("d=%d\n",d/=5); //divide assigment operator
+    return 0;
+
+}
