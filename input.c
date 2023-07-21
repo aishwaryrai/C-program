2b8912ca45c41c1d68683e72b375a511e477eb17
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-07-21 23:07:31 +0530

    first

---
+++ input.c
@@ -0,0 +1,15 @@
+/*Program for assigning input through keyboard during execution*/
+
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    float c;
+    char d;
+
+    printf("Enter the value of a, b, c, d");
+    scanf("%d%d%f%c", &a,&b,&c,&d);
+
+return 0;
+
+}
