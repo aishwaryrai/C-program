ecf781f93ac79f70dfe629df29faae3d71459655
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-09-03 04:34:46 +0530

    .

---
+++ whileloop.c
@@ -0,0 +1,14 @@
+//In While loop statement is executed after condition is tested.
+
+#include <stdio.h>
+int main(){
+    int i = 0 ;
+    while (i < 45) {
+        printf( " %d \n ", i);
+        i = i + 1;
+
+    }
+    return 0;
+    
+
+}
