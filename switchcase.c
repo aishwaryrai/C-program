93b74d8e4d0a7c08872f0e20a35203b6e4443e48
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-09-02 14:20:15 +0530

    .

--- switchcase.c
+++ switchcase.c
@@ -7,10 +7,15 @@ int main(){
     {
     case (18):
         printf(" Your age is 18. ");
+        break;
     case (28):
         printf(" Your age is 28. ");
+        break;
+
     case (40):
         printf(" Your age is 40. ");
+        break;
+        
     
     
     default:
