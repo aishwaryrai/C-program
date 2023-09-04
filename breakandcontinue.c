8db6e5ab45b742e37e69e654c603b52e741f634d
Aishwary Rai <121277048+aishwaryrai@users.noreply.github.com>
2023-09-05 01:51:20 +0530

    ..

---
+++ breakandcontinue.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main (){
+    int i , num ;
+    for (i=0; i<6; i++)
+    {
+        printf(" %d\n Enter a num\n " , i );
+        scanf(" % d" , &num );
+        /* if (num>8){
+            break;
+
+        }*/
+        if (num>10){
+            continue;
+        }
+
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+        printf("Asdhgwvdhd");
+
+
+
+    }
+    return 0;
+
+}
